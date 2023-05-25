ba31ae73a5f5627a78242fc229ef932c04197410
Mhmd12z <98625548+Mhmd12z@users.noreply.github.com>
2023-05-25 15:01:07 +0300

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let megaMenu = document.getElementById("mega-menu");
 let menuBool = true;
+// MegaMenu Presence on click
 function present() {
   if (menuBool == true) {
     megaMenu.style.opacity = 1;
@@ -12,10 +13,10 @@ function present() {
     menuBool = true;
   }
 }
+// Return to top function
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   var scrollToTopBtn = document.getElementById("scrollToTopBtn");
   if (
@@ -27,27 +28,11 @@ function scrollFunction() {
     scrollToTopBtn.style.display = "none";
   }
 }
-
-// Function to scroll to the top when the button is clicked
 function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-// let cardHover=document.querySelector(".card");
-// let cardActive = true;
-// function freeze(){
-//   if (cardActive == true) {
-//     cardHover.style.backgroundColor="#aad4e2";
-//     cardHover.style.color="#01265a";
-//     cardActive = false;
-//   } else {
-//     cardHover.style.backgroundColor="#01265a";
-//     cardHover.style.color="white";
-//     cardActive = true;
-//   }
-// }
-
+// Change card color when click on it
 function toggleBackgroundColor(element) {
   if (element.style.backgroundColor === "") {
     element.style.backgroundColor = "#aad4e2";
@@ -55,6 +40,7 @@ function toggleBackgroundColor(element) {
     element.style.backgroundColor = "";
   }
 }
+// Timer For Event
 function startTimer(duration, display) {
   var timer = duration,
     hours,
@@ -76,18 +62,18 @@ function startTimer(duration, display) {
     }
   }, 1000);
 }
-
 window.onload = function () {
   var duration = 23600;
   var display = document.getElementById("timer");
   startTimer(duration, display);
 };
 
-function submitt(){
+// Submit Button on payments page
+function submitBtn() {
   let inputs = document.getElementsByTagName("input");
   let button = document.getElementById("btn-submit");
-  for(let i=0;i<inputs.length;i++){
-    if(inputs[i].value===""){
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === "") {
       alert("Please Fill All The Fields");
       event.preventDefault();
       return;
@@ -95,29 +81,50 @@ function submitt(){
   }
   displayMessage();
 }
-  function submitt2(){
-    let inputs = document.getElementsByTagName("input");
-    let button = document.getElementById("confirm-donation");
-    let email=document.getElementById("email");
-    for(let i=0;i<inputs.length;i++){
-      if(inputs[i].value===""){
-        alert("Please Fill All The Fields || put correct email");
-        event.preventDefault();
-        return;
-      }
+// Continue Button on Main Page
+function continueBtn() {
+  let inputs = document.getElementsByTagName("input");
+  let button = document.getElementById("confirm-donation");
+  let email = document.getElementById("email");
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === "") {
+      alert("Please Fill All The Fields || put correct email");
+      event.preventDefault();
+      return;
     }
-    goToAnotherPage();
+  }
+  goToAnotherPage();
 }
-function goToAnotherPage(){
+// Go To Payments page upon clicking continue
+function goToAnotherPage() {
   window.location.href = "/payments.html";
 }
-function displayMessage(){
-  let message=document.querySelector(".finish .container");
-  let paymentPage=document.getElementById("payments");
-  let visa= document.getElementsByClassName("visa")[0];
-  message.style.zIndex=99;
-  message.style.display="flex";
-  paymentPage.style.opacity=0;
-  visa.style.opacity=0;
+// Display Successful message up clicking submit on payment page
+function displayMessage() {
+  let message = document.querySelector(".finish .container");
+  let paymentPage = document.getElementById("payments");
+  let visa = document.getElementsByClassName("visa")[0];
+  message.style.zIndex = 99;
+  message.style.display = "flex";
+  paymentPage.style.opacity = 0;
+  visa.style.opacity = 0;
   event.preventDefault();
+}
+// Change number on visa upon changing it in the input
+function changeNumber() {
+  let number = document.getElementById("card-number");
+  let inputNumber = document.getElementsByName("input-number")[0].value;
+  number.innerHTML = inputNumber;
+}
+// Change holder on visa upon changing it in the input
+function changeHolder() {
+  let holder = document.getElementById("card-holder");
+  let inputHolder = document.getElementsByName("input-holder")[0].value;
+  holder.innerHTML = inputHolder;
+}
+// Change CVV on visa upon changing it in the input
+function changeCVV() {
+  let cvv = document.getElementById("cvv");
+  let inputCvv = document.getElementsByName("input-cvv")[0].value;
+  cvv.innerHTML = inputCvv;
 }
