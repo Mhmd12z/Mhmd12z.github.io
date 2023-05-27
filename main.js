c0e7e31516000c54bfcac01382447ed68e3a3c56
Mhmd12z <98625548+Mhmd12z@users.noreply.github.com>
2023-05-27 17:06:24 +0300

    main.js

--- main.js
+++ main.js
@@ -42,28 +42,17 @@ function toggleBackgroundColor(element) {
 }
 // Timer For Event
 function startTimer(duration, display) {
-  var timer = duration,
-    hours,
-    minutes,
-    seconds;
+  var timer = duration;
   setInterval(function () {
-    hours = parseInt(timer / 3600, 10);
-    minutes = parseInt((timer % 3600) / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = hours + ":" + minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
+    display.textContent = timer;
+    if (--timer <= 0) {
+      clearInterval(interval);
     }
   }, 1000);
 }
+
 window.onload = function () {
-  var duration = 23600;
+  var duration = 1600; // Set the duration in seconds
   var display = document.getElementById("timer");
   startTimer(duration, display);
 };
